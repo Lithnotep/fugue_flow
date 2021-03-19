55970c9121ad6840483bc2f67c465aa3a6eb4f50
Max Mitrani <mjmitrani168@gmail.com>
2021-03-19 14:22:05 -0600

    add function session login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
 class SessionsController < ApplicationController
 
+    def new
+        @user = User.new
+    end
+    
     def create
-        @user = User.find_by(username: params[:session][:email])
+        @user = User.find_by(username: params[:session][:username])
     end
 
 end
