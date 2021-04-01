fa5e6e5fcb23cdc8b5d649d3164b6f1398fa50a3
Max Mitrani <mjmitrani168@gmail.com>
2021-04-01 12:27:09 -0600

    add passing dream create test

--- spec/models/dream_spec.rb
+++ spec/models/dream_spec.rb
@@ -10,11 +10,14 @@ RSpec.describe Dream, type: :model do
     it {should belong_to :user}
   end
   
-  # describe 'dream' do
-  #   it 'can be created as dream' do
-  #     user = User.create(description: 'Megaman', password: 'password')
+  describe 'dream' do
+    it 'can be created' do
+      user = User.create(username: 'Megaman', password: 'password')
+      dream = Dream.create(description: 'What a great Dream!', date: Date.new(2015, 12, 8), user_id: user.id)
       
-  #     expect(user.username).to eq('Megaman')
-  #   end
-  # end
+      expect(user.username).to eq('Megaman')
+      expect(dream.description).to eq('What a great Dream!')
+      expect(dream.date.strftime("%a, %d %b %Y")).to eq('Tue, 08 Dec 2015')
+    end
+  end
 end
